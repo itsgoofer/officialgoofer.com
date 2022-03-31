3c0a28dfeee2d25d9c436ea8787a18f2d51a9c29
Omar Fouad <omarfouad.net@gmail.com>
2022-03-31 12:30:34 +0200

    Updated website to use the new notify api by gooferville.

--- _js/main.js
+++ _js/main.js
@@ -2,24 +2,11 @@ var axios = require('axios')
 
 module.exports = {
 	sendNotification: function(type) {
-		let city = ""
-		let country = ""
 
-		axios.get('https://json.geoiplookup.io').then((data)=> {
-      this.notify(type, data.data)
+		axios.post('https://api.gooferville.com/notify/trigger', {
+			"discord_hooks": ["https://discord.com/api/webhooks/956598012954673162/JmmbBreSRt-8k2r-0QExUqsDCP3Ph0actwEx1NWwbnCkyS7gICUpKsTtAZDG4JW59l26"],
+			"ifttt_hook": "https://maker.ifttt.com/trigger/visit/with/key/b671wE8bjS_ZWi8FMMmD-h",
+			"title": "officialgoofer.com - " + type
 		})
 	},
-	notify: function(type, data) {
-		axios.get('https://maker.ifttt.com/trigger/visit/with/key/b671wE8bjS_ZWi8FMMmD-h', {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      },
-			params: {
-				value1: type,
-				value2: data.country_name + " - " + data.region + ", " + data.city,
-				value3: ""
-			}
-		})
-	}
 }
